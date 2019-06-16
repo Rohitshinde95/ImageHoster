2417b8da001c3f8e6c79080d1114d82426087b98
Rohit Shinde <rohitshinde803@gmail.com>
2019-06-16 21:14:37 +0530

    Password strength check added
    - Password must contain at least 1 alphabet, 1 number & 1 special character

--- ImageHoster/src/main/java/ImageHoster/service/UserService.java
+++ ImageHoster/src/main/java/ImageHoster/service/UserService.java
@@ -5,6 +5,9 @@ import ImageHoster.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service
 public class UserService {
 
@@ -30,6 +33,23 @@ public class UserService {
         } else {
             return null;
         }
+    }
+
+    //Validate password strength
+    public boolean validatePassword(String password){
+        Pattern letter =  Pattern.compile(".*[a-zA-Z]+.*");
+        Pattern digit =  Pattern.compile("(.)*(\\d)(.)*");
+        Pattern special = Pattern.compile("[a-zA-Z0-9]*");
+
+        Matcher hasLetter = letter.matcher(password);
+        Matcher hasDigit = digit.matcher(password);
+        Matcher hasSpecial = special.matcher(password);
+
+        boolean atleastOneLetter=hasLetter.find();
+        boolean atleastOneDigit=hasDigit.find();
+        boolean atleastOneSpecial=!hasSpecial.matches();
+
+        return (atleastOneLetter && atleastOneDigit && atleastOneSpecial);
     }
 
 }
